6fbdbf12ffe0596fb945b80d8f4f8fb781aa7f2e
dasgoutam <goutam2475@gmail.com>
2025-06-28 13:46:07 +0200

    test deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/personal-website",
 };
 
 export default nextConfig;
