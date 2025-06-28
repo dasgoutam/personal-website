fe30484665d973127f4c56b5d03baa8ed60a5f0e
dasgoutam <goutam2475@gmail.com>
2025-06-28 14:52:52 +0200

    change basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/personal-website",
 };
 
 export default nextConfig;
