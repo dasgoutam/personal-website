5cde8a920b8b37cc2a98bc83d5118f736914b65c
dasgoutam <goutam2475@gmail.com>
2025-06-28 09:15:19 +0200

    add image for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Image from 'next/image';
+
 export default function Home() {
-    return <div>This is my new home</div>;
+    return <div>
+      <div>
+      <Image
+                src="/images/profile_picture.png"
+                alt="profile_picture"
+                width={180}
+                height={180}
+                className="rounded-full bg-blue-200"
+            />
+      </div>
+      <p>This is my new home</p>
+    </div>;
 }
